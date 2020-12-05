62d1e14b5023835910931be9a19a167d5f583c08
kamilaolech <75523948+kamilaolech@users.noreply.github.com>
2020-12-05 15:31:05 +0100

    Update zadanie8_13.py

--- zadanie8_13.py
+++ zadanie8_13.py
@@ -1,5 +1,13 @@
-atomy = [{'nazwa': 'wodor', 'grupa': '1', 'okres': '1'},
- {'nazwa': 'hel', 'grupa': '2', 'okres': '1'}]
+atomy = [{'nazwa': 'wodor', 'grupa': 1, 'okres': 1},
+ {'nazwa': 'hel', 'grupa': 2, 'okres': 1}]
 
 for i in atomy:
-    print(i['nazwa'] + ' - grupa: ' + i['grupa'] + ' okres: ' + i['okres'])
+    print(i['nazwa'] + ' - grupa: ' + str(i['grupa']) + ' okres: ' + str(i['okres']))
+
+suma_walencyjnych=0
+for i in atomy:
+    suma_walencyjnych+=i['grupa']
+  
+print('Suma elektronow walencyjnych: ' + str(suma_walencyjnych))
+  
+ 
