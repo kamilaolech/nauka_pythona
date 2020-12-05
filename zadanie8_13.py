fbee4c25e7296586c0cda65c4e0afdef35ce95b1
kamilaolech <kamilaolech@users.noreplay.github.com>
2020-12-05 14:55:30 +0100

    zadanie8_13

---
+++ zadanie8_13.py
@@ -0,0 +1,5 @@
+atomy = [{'nazwa': 'wodor', 'grupa': '1', 'okres': '1'},
+ {'nazwa': 'hel', 'grupa': '2', 'okres': '1'}]
+
+for i in atomy:
+    print(i['nazwa'] + ' - grupa: ' + i['grupa'] + ' okres: ' + i['okres'])
