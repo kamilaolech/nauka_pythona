13f46de3a106e594f77955c79a34ef55e763f8bb
kamilaolech <75523948+kamilaolech@users.noreply.github.com>
2020-12-06 12:31:24 +0100

    Update ukryte_haslo.py

--- ukryte_haslo.py
+++ ukryte_haslo.py
@@ -4,4 +4,4 @@ if len(haslo) < 3:
 else:
     ilosc_gwiazdek = len(haslo[1:-1])
     gwiazdy = ilosc_gwiazdek * "*"
-    print(haslo[0] + gwiady + haslo[-1])
+    print(haslo[0] + gwiazdy + haslo[-1])
