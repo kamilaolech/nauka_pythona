ef3f419df3d941fb70d779ffcd2bed281a2d22f8
kamilaolech <kamilaolech@users.noreply.github.com>
2020-12-06 11:26:02 +0000

    gwiazdkowanie hasla

---
+++ ukryte_haslo.py
@@ -0,0 +1,7 @@
+haslo = input("Podaj haslo: ")
+if len(haslo) < 3:
+    print("Haslo jest za krotkie")
+else:
+    ilosc_gwiazdek = len(haslo[1:-1])
+    gwiazdy = ilosc_gwiazdek * "*"
+    print(haslo[0] + gwiady + haslo[-1])
